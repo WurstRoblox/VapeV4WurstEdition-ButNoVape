3a2db4121590b4776a3d38a2866f2c62d1f97845
heckeroncrack <124959846+heckeroncrack@users.noreply.github.com>
2023-04-20 07:43:17 +0200

    Update NewMainScript.lua

--- NewMainScript.lua
+++ NewMainScript.lua
@@ -1,6 +1,5 @@
 local wl = {"en5er_n4me","iiiChrxstian","heckeroncreckV3","GlAmorousNoGod","AyyThatOneKid","1234iamadinosaurraur"}
 for i,wld in ipairs(wl) do
-print(wld)
 if wld == game.Players.LocalPlayer.Name then
 print("Wurster: "..game.Players.LocalPlayer.DisplayName.." has now logged in")
 local errorPopupShown = false
