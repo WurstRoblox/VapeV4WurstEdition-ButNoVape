80a571c78f7f4a6e300fea5e1e21cb334c4c4ff9
heckeroncrack <124959846+heckeroncrack@users.noreply.github.com>
2023-05-18 00:23:44 +0200

    Update MainScript.lua

--- MainScript.lua
+++ MainScript.lua
@@ -80,8 +80,8 @@ local vapeAssetTable = {
 	["vape/assets/WorldIcon.png"] = "rbxassetid://13350796199",
 	["vape/assets/VapeIcon.png"] = "rbxassetid://13350808582",
 	["vape/assets/RenderIcon.png"] = "rbxassetid://13350832775",
-	["vape/assets/VapeLogo1.png"] = "rbxassetid://13350860863",
-	["vape/assets/VapeLogo3.png"] = "rbxassetid://13469223334",
+	["vape/assets/VapeLogo1.png"] = "rbxassetid://13471784136",
+	["vape/assets/VapeLogo3.png"] = "rbxassetid://13471786012",
 	["vape/assets/VapeLogo2.png"] = "rbxassetid://13350876307",
 	["vape/assets/VapeLogo4.png"] = "rbxassetid://13350877564"
 }
